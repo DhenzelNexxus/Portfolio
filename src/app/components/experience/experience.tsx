c48555000d547aa8b9eff16b2b74ceadc54adb5c
Dhenzel Lima Ribeiro <dhenzellimaribeiro@gmail.com>
2024-01-04 21:11:15 -0300

    feat: config experience section

---
+++ src/app/components/experience/experience.tsx
@@ -0,0 +1,14 @@
+import { SectionTitle } from "../sectionTitle/section-title"
+import "./experience.scss"
+
+export function Experience(){
+    return(
+        <div className="experience">
+        <SectionTitle text="Experiência"/>
+        <p>Estágio de desenvolvimento - Polibras SoftWare - 5 meses</p>
+        <div className="experience-time">
+
+        </div>
+      </div>
+    )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,12 @@
 import { Header } from './components/Header/header'
+import { Experience } from './components/experience/experience'
 import "./styles/home.scss"
 
 export default function Home() {
   return (
     <main className='container'>
       <Header />
-      <div className="experience">
-        <h3>Experiencia</h3>
-        <p>Estágio de desenvolvimento - Polibras SoftWare - 5 meses</p>
-        <div className="experience-time"></div>
+      <Experience />
         <div className="infos">
           <h3>Linguas</h3>
           <div className="language-info">
@@ -19,13 +17,12 @@ export default function Home() {
           <div className="educational-info">
             <span></span>
             <span>Curso Tecnico de Informatica - Escola Educacional de Ensino Profissional Edson Queiroz</span>
-          </div>
+        </div>
+        </div>
           <div className="buttons">
             <div className="social"></div>
             <button>contact me</button>
           </div>
-        </div>
-      </div>
     </main>
   )
 }
