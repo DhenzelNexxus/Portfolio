281a932f78853ee25de26e97ea699535c1b8c54e
DhenzelNexus <dhenzellimaribeiro@gmail.com>
2024-01-06 09:30:09 -0300

    FEAT: improving organization and add new experience

--- src/app/components/experience/experience.tsx
+++ src/app/components/experience/experience.tsx
@@ -8,12 +8,14 @@ export function Experience(){
     return(
         <div className="experience">
           <SectionTitle text="Experience"/>
-          <p>5 mouths working as a Software Developer, in companys such as Polibras Software.</p>
+          <p>5 months interning as a software developer at Polibras Software, a large company in the sales force area.</p>
           <div className="experience-time">
-            <Skill image="/python.png" measure={3} />
+            <Skill image="/git.png" measure={3} />
+            <Skill image="/python2.png" measure={3} />
+            <Skill image="/django.jpg" measure={2} />
             <Skill image="/react.png" measure={2} />
-            <Skill image="/ts.png" measure={1} />
             <Skill image="/js.png" measure={2}/>
+            <Skill image="/ts.png" measure={1} />
           </div>
         </div>
     )
